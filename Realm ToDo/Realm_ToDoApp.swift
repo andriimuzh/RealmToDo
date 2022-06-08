0956afffa76f3be139110cb51c3cd8e58daf457a
andriimuzh <muzhandriy@gmail.com>
2022-06-08 11:21:59 +0300

    Initial Commit

---
+++ Realm ToDo/Realm_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_ToDoApp.swift
+//  Realm ToDo
+//
+//  Created by Andrii Muzh on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Realm_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
